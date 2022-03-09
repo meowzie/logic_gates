3b2ac84664a9e56d21f89f06c2192f1a24e72679
meowzie <appenge2@gmail.com>
2022-03-09 17:38:41 +0200

    Removed redundancy in conditions

--- logic_gates.rb
+++ logic_gates.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def input_checker(array)
   array.all? { |item| item.is_a?(TrueClass) || item.is_a?(FalseClass) }
 end
@@ -12,7 +14,7 @@ end
 def or_method(*booleans)
   booleans.flatten!
   return 'Invalid input' unless input_checker(booleans)
-  
+
   booleans.any?
 end
 
@@ -43,15 +45,15 @@ end
 
 def method_getter
   prompt =
-  <<~EOS
-  Choose what type of logic gate you would like.
-  A = AND
-  O = OR
-  N = NOT
-  NA = NAND
-  NO = NOR
-  X = XOR
-  EOS
+    <<~EOS
+      Choose what type of logic gate you would like.
+      A = AND
+      O = OR
+      N = NOT
+      NA = NAND
+      NO = NOR
+      X = XOR
+    EOS
 
   puts prompt
   allowed = %w[a o n na no x]
@@ -62,33 +64,33 @@ end
 def value_getter
   puts 'What values would you like to test? Enter T for True and F for False'
   puts "\nAll gates take unlimited values, except for NOT (which only takes one)"
-  puts "\nYou may type your values out in one line." 
+  puts "\nYou may type your values out in one line."
 
   values = gets.chomp.downcase
   values = values.split('')
   values.each { |value| values.delete(value) unless ('a'..'z').to_a.include?(value) }
-  return "Invalid input or inputs" unless values.all? { |value| value == 't' || value == 'f' }
+  return 'Invalid input or inputs' unless values.all? { |value| %w[t f].include?(value) }
 
-  values.map { |value| value == 't' ? true : false }
+  values.map { |value| value == 't' }
 end
 
 def combiner
   values = value_getter
 
   result = case method_getter
-  when 'a'
-    and_method(values)
-  when 'o'
-    or_method(values)
-  when 'n'
-    not_method(values)
-  when 'na'
-    nand_method(values)
-  when 'no'
-    nor_method(values)
-  when 'x'
-    xor_method(values)
-  end
+           when 'a'
+             and_method(values)
+           when 'o'
+             or_method(values)
+           when 'n'
+             not_method(values)
+           when 'na'
+             nand_method(values)
+           when 'no'
+             nor_method(values)
+           when 'x'
+             xor_method(values)
+           end
 
   puts result
 end
